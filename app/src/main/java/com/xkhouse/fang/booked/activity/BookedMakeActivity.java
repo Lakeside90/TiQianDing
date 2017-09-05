9e0687dcc52205e9975e8972fdd0f48d397285d9
demozi <wujian199011@163.com>
2017-09-05 20:31:59 +0800

    在线预定+在线买单UI

---
+++ app/src/main/java/com/xkhouse/fang/booked/activity/BookedMakeActivity.java
@@ -0,0 +1,90 @@
+package com.xkhouse.fang.booked.activity;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.xkhouse.fang.R;
+import com.xkhouse.fang.app.activity.AppBaseActivity;
+import com.xkhouse.fang.user.adapter.DSDetailAdapter;
+import com.xkhouse.fang.widget.ScrollListView;
+
+/**
+ * 在线预定
+ */
+public class BookedMakeActivity extends AppBaseActivity {
+	
+	private ImageView iv_head_left;
+	private TextView tv_head_title;
+
+
+
+
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+
+        fillData();
+	}
+	
+	
+	@Override
+	protected void setContentView() {
+		setContentView(R.layout.activity_booked_make);
+	}
+
+	@Override
+	protected void init() {
+		super.init();
+	}
+
+	@Override
+	protected void findViews() {
+		initTitle();
+	}
+
+
+	private void initTitle() {
+		tv_head_title = (TextView) findViewById(R.id.tv_head_title);
+		tv_head_title.setText("在线预定");
+		iv_head_left = (ImageView) findViewById(R.id.iv_head_left);
+        iv_head_left.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				
+				finish();
+			}
+		});
+	}
+	
+	@Override
+	protected void setListeners() {
+
+	}
+
+	@Override
+	public void onClick(View v) {
+		super.onClick(v);
+        switch (v.getId()){
+
+
+        }
+	}
+	
+	
+	private void fillData(){
+
+
+	}
+	
+
+
+}
---
+++ app/src/main/java/com/xkhouse/fang/booked/activity/PayMakeActivity.java
@@ -0,0 +1,85 @@
+package com.xkhouse.fang.booked.activity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.xkhouse.fang.R;
+import com.xkhouse.fang.app.activity.AppBaseActivity;
+
+/**
+ * 在线买单
+ */
+public class PayMakeActivity extends AppBaseActivity {
+	
+	private ImageView iv_head_left;
+	private TextView tv_head_title;
+
+
+
+
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+
+        fillData();
+	}
+	
+	
+	@Override
+	protected void setContentView() {
+		setContentView(R.layout.activity_pay_make);
+	}
+
+	@Override
+	protected void init() {
+		super.init();
+	}
+
+	@Override
+	protected void findViews() {
+		initTitle();
+	}
+
+
+	private void initTitle() {
+		tv_head_title = (TextView) findViewById(R.id.tv_head_title);
+		tv_head_title.setText("在线买单");
+		iv_head_left = (ImageView) findViewById(R.id.iv_head_left);
+        iv_head_left.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				
+				finish();
+			}
+		});
+	}
+	
+	@Override
+	protected void setListeners() {
+
+	}
+
+	@Override
+	public void onClick(View v) {
+		super.onClick(v);
+        switch (v.getId()){
+
+
+        }
+	}
+	
+	
+	private void fillData(){
+
+
+	}
+	
+
+
+}
